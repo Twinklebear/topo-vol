b0344c8104a32080c38b064532352e4392d06be3
Will Usher <willusher.life@gmail.com>
2017-04-20 19:13:28 -0600

    fix branch placement, bad dist. calc
    Remove commented out code

--- topo-volume/tree_widget.cpp
+++ topo-volume/tree_widget.cpp
@@ -38,13 +38,13 @@ std::ostream& operator<<(std::ostream &os, const Branch &b) {
 glm::vec2 TreeNode::get_input_slot_pos(const size_t segment) const {
 	auto fnd = std::find(entering_branches.begin(), entering_branches.end(), segment);
 	assert(fnd != entering_branches.end());
-	const size_t slot = std::distance(fnd, entering_branches.begin());
+	const size_t slot = std::distance(entering_branches.begin(), fnd);
 	return glm::vec2(ui_pos.x, ui_pos.y + ui_size.y * static_cast<float>(slot + 1) / (entering_branches.size() + 1));
 }
 glm::vec2 TreeNode::get_output_slot_pos(const size_t segment) const {
 	auto fnd = std::find(exiting_branches.begin(), exiting_branches.end(), segment);
 	assert(fnd != exiting_branches.end());
-	const size_t slot = std::distance(fnd, exiting_branches.begin());
+	const size_t slot = std::distance(exiting_branches.begin(), fnd);
 	return glm::vec2(ui_pos.x + ui_size.x,
 			ui_pos.y + ui_size.y * static_cast<float>(slot + 1) / (exiting_branches.size() + 1));
 }
@@ -210,15 +210,11 @@ void TreeWidget::draw_ui() {
 	// Display the graph links, in background
 	draw_list->ChannelsSetCurrent(0);
 	for (const auto &b : branches) {
-		if (b.start_node >= nodes.size() || b.end_node >= nodes.size()) {
-			std::cout << "WARNING: branch missing start or end pt!?\n";
-			continue;
-		}
 		const TreeNode &start = nodes[b.start_node];
 		const TreeNode &end = nodes[b.end_node];
 		const glm::vec2 p1 = offset + start.get_output_slot_pos(b.segmentation_id);
 		const glm::vec2 p2 = offset + end.get_input_slot_pos(b.segmentation_id);
-		draw_list->AddLine(p1, p2, ImColor(255 * 0.8f, 255 * 0.8f, 255 * 0.1f));
+		draw_list->AddLine(p1, p2, ImColor(0.8f, 0.8f, 0.1f));
 	}
 
 	const glm::vec2 NODE_WINDOW_PADDING(8);
@@ -261,91 +257,6 @@ void TreeWidget::draw_ui() {
 
 	ImGui::End();
 }
-#if 0
-void TreeWidget::draw_ui() {
-	if (!ImGui::Begin("Tree Widget")) {
-		ImGui::End();
-		return;
-	}
-
-	ImGui::Text("Contour/Split/Merge Tree");
-	ImGui::Text("Left click to select branch, Ctrl-click to select multiple");
-	if (ImGui::Button("Clear Selection")) {
-		selected_segmentations.clear();
-	}
-
-	ImGui::SliderFloat("Zoom Tree", &zoom_amount, 1.0f, 35.f);
-
-	const glm::vec2 canvas_size(ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y * zoom_amount);
-	const glm::vec2 mouse_pos = glm::vec2(ImGui::GetIO().MousePos.x, ImGui::GetIO().MousePos.y);
-	ImGui::BeginChild("tree", canvas_size);
-
-	const glm::vec2 canvas_pos(ImGui::GetCursorScreenPos().x, ImGui::GetCursorScreenPos().y);
-	const glm::vec2 view_scale(canvas_size.x - 16, -canvas_size.y / img_range.y);
-	const glm::vec2 view_offset(canvas_pos.x + 8, canvas_pos.y + canvas_size.y);
-	ImDrawList *draw_list = ImGui::GetWindowDrawList();
-	draw_list->AddRect(canvas_pos, canvas_pos + canvas_size, ImColor(128, 128, 128));
-
-	for (const auto &b : display_tree.branches) {
-		const Branch &seg_branch = branches[b.first];
-
-		const glm::vec2 start = display_tree.points[b.second.start] * view_scale + view_offset;
-		const glm::vec2 end = display_tree.points[b.second.end] * view_scale + view_offset;
-		if (point_on_line(start, end, mouse_pos)) {
-			if (ImGui::IsMouseClicked(0)) {
-				const bool was_selected = std::find(selected_segmentations.begin(), selected_segmentations.end(),
-						b.first) != selected_segmentations.end();
-				if (!ImGui::GetIO().KeyCtrl) {
-					selected_segmentations.clear();
-				}
-				auto fnd = std::find(selected_segmentations.begin(), selected_segmentations.end(), b.first);
-				if (fnd != selected_segmentations.end()) {
-					selected_segmentations.erase(fnd);
-				} else if (!was_selected) {
-					selected_segmentations.push_back(b.first);
-				}
-			}
-
-			std::string incident_list, exiting_list;
-			for (const auto &x : seg_branch.entering_branches) {
-				incident_list += std::to_string(x) + ", ";
-			}
-			for (const auto &x : seg_branch.exiting_branches) {
-				exiting_list += std::to_string(x) + ", ";
-			}
-			ImGui::SetTooltip("Segmentation %lu, range [%.3f, %.3f]\n# Incident %lu - [%s]\n# Exiting %lu - [%s]",
-					b.first, seg_branch.start_val, seg_branch.end_val, seg_branch.entering_branches.size(),
-					incident_list.c_str(), seg_branch.exiting_branches.size(), exiting_list.c_str());
-		}
-
-		// Color format is AABBGGRR
-		uint32_t color = 0xff1fefef;
-		if (!selected_segmentations.empty()) {
-			color = 0x4f1fefef;
-			auto fnd = std::find(selected_segmentations.begin(), selected_segmentations.end(), b.first);
-			if (fnd != selected_segmentations.end()) {
-				color = 0xffffffff;
-			} else {
-				auto fnd = std::find_first_of(seg_branch.entering_branches.begin(), seg_branch.entering_branches.end(),
-						selected_segmentations.begin(), selected_segmentations.end());
-				if (fnd != seg_branch.entering_branches.end()) {
-					color = 0xffff0000;
-				}
-
-				fnd = std::find_first_of(seg_branch.exiting_branches.begin(), seg_branch.exiting_branches.end(),
-						selected_segmentations.begin(), selected_segmentations.end());
-				if (fnd != seg_branch.exiting_branches.end()) {
-					color = 0xff0000ff;
-				}
-			}
-		}
-		draw_list->AddLine(start, end, color);
-	}
-	ImGui::EndChild();
-
-	ImGui::End();
-}
-#endif
 const std::vector<uint32_t>& TreeWidget::get_selection() const {
 	return selected_segmentations;
 }
@@ -356,85 +267,4 @@ struct VecComparator {
 			|| (a.x == b.x && a.y == b.y && a.z < b.z);
 	}
 };
-#if 0
-void TreeWidget::build_ui_tree() {
-	// Build a map of the 3D point to the graph point and use that to do the lookup for point ids
-	std::map<glm::uvec3, size_t, VecComparator> point_map;
-	std::set<float> fcn_vals;
-	for (const auto &b : branches) {
-		display_tree.branches[b.segmentation_id].segmentation = b.segmentation_id;
-
-		// Look and see if a point for our start point has been made already by the branches
-		// entering us
-		size_t start_pt = display_tree.points.size();
-#if 0
-		// TODO: Fix the shared vertex finding
-		for (const auto &x : b.entering_branches) {
-			auto fnd = display_tree.branches.find(x);
-			if (fnd != display_tree.branches.end()) {
-				start_pt = fnd->second.end;
-				break;
-			}
-		}
-#endif
-		if (start_pt == display_tree.points.size()) {
-			display_tree.points.push_back(glm::vec2(0.f, b.start_val));
-		}
-
-		display_tree.branches[b.segmentation_id].start = start_pt;
-#if 0
-		for (const auto &x : b.entering_branches) {
-			display_tree.branches[x].end = start_pt;
-		}
-#endif
-
-
-		// Look and see if a point for our end point has been made already by the branches
-		// exiting us
-		size_t end_pt = display_tree.points.size();
-#if 0
-		for (const auto &x : b.exiting_branches) {
-			auto fnd = display_tree.branches.find(x);
-			if (fnd != display_tree.branches.end()) {
-				end_pt = fnd->second.end;
-				break;
-			}
-		}
-#endif
-		if (end_pt == display_tree.points.size()) {
-			display_tree.points.push_back(glm::vec2(0.f, b.end_val));
-		}
-
-		display_tree.branches[b.segmentation_id].end = end_pt;
-#if 0
-		for (const auto &x : b.exiting_branches) {
-			display_tree.branches[x].start = end_pt;
-		}
-#endif
-
-		fcn_vals.insert(b.start_val);
-		fcn_vals.insert(b.end_val);
-	}
-
-	std::cout << "# of nodes in tree = " << display_tree.points.size() << "\n";
-
-	// Pass through all points at each function value and space them out nicely
-	for (const auto &f : fcn_vals) {
-		const size_t n = std::count_if(display_tree.points.begin(), display_tree.points.end(),
-				[&](const glm::vec2 &v) { return v.y == f; });
-		// This probably shouldn't happen, maybe precision issues?
-		if (n == 0) {
-			continue;
-		}
-
-		float i = 0;
-		for (auto &p : display_tree.points) {
-			if (p.y == f) {
-				p.x = i / n;
-				++i;
-			}
-		}
-	}
-}
-#endif
 
